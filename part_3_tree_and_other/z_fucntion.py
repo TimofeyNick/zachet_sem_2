bd3e64d10bd8c8e9f6895c4a4323ef706a9cde89
TimofeyNick <miryashkin@yandex.ru>
2017-05-23 07:35:24 +0300

    Update z_fucntion.py

--- part_3_tree_and_other/z_fucntion.py
+++ part_3_tree_and_other/z_fucntion.py
@@ -4,7 +4,7 @@ z = [0]*n
 left = 0
 right = 0
 for i in range(1,n):
-    x = 0
+    x = min(z[i-left], right - i + 1) i <=right else 0 
     while x + i < n and s[x] == s[x + i]:
         x += 1
     z[i] = x
